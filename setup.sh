4bf6c677344763f79068e1a6dbf3ff49bb892026
Abi Hafshin Alfarouq <abi@hafs.in>
2022-11-02 15:45:45 +0700

    mount state dir

--- setup.sh
+++ setup.sh
@@ -1,16 +1,31 @@
 #!/bin/sh
 set -ex
 
+ROOT_PATH="/.bottlerocket/rootfs"
+
 # The name of the disk we want to manage
-DISK=/.bottlerocket/rootfs/dev/nvme2n1
+DISK="${ROOT_PATH}/dev/nvme2n1"
+
 # Mounts from this mount point will propagate accross mount namespaces
-MOUNT_POINT=/.bottlerocket/rootfs/mnt/ephemeral
+MOUNT_POINT="${ROOT_PATH}/mnt/ephemeral"
 
 # Format disk
-mkfs.ext4 -F ${DISK}
+mkfs.ext4 -F "${DISK}"
 
 # We make sure the target mount points exist
-mkdir -p $MOUNT_POINT
+mkdir -p "${MOUNT_POINT}"
 
 # Always mount the partitions
-mount ${DISK} $MOUNT_POINT
+mount "${DISK}" "${MOUNT_POINT}"
+
+for state in containerd docker kubelet ; do
+  state_dir="${ROOT_PATH}/var/lib/${state}"
+  if [ -d "${state_dir}" ]; do
+    mv "${state_dir}" "${MOUNT_POINT}/"
+  else
+    mkdir -p "${MOUNT_POINT}/${state}"
+  fi
+  mkdir "${state_dir}"
+  mount --rbind "${MOUNT_POINT}/${state}" "${state_dir}"
+  mount --make-rshared "${state_dir}"
+done
